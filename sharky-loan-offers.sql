58b12eb52daf19219d487de212814d3095d4bec4
Bert Hoffmann <rezenzeffekt@gmail.com>
2025-05-23 23:11:54 +0200

    edge cases are not your friend

--- sharky-loan-offers.sql
+++ sharky-loan-offers.sql
@@ -4,85 +4,38 @@ with txs as (
 select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[0]:instructions[2]:parsed:info:destination as escrow_token
 from solana.core.fact_transactions
 where (contains (log_messages[1],'OfferLoan') or contains (log_messages[5],'OfferLoan'))
-and block_timestamp > current_date() - interval '{{hours}} hours'
+and block_timestamp > current_date() - interval '{{day}} day'
 and succeeded
 and signers[2] is null
-and (instructions[0]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM' or instructions[2]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM')
-union all
--- 2 loans offered 
-select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[0]:instructions[2]:parsed:info:destination as escrow_token
-from solana.core.fact_transactions
-where contains (log_messages[1],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
-and succeeded
-and signers[2] is not null
-and signers[3] is null
-and instructions[0]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
-union all
-select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[1]:instructions[2]:parsed:info:destination as escrow_token
-from solana.core.fact_transactions
-where contains (log_messages[1],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
-and succeeded
-and signers[2] is not null
-and signers[3] is null
-and instructions[0]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
+and (instructions[0]:accounts[6] = '5274Bqe6RmGkxw6E6M7pYpEkJFh4VS7mcZQe2mQ6ZRLC' or instructions[2]:accounts[6] = '5274Bqe6RmGkxw6E6M7pYpEkJFh4VS7mcZQe2mQ6ZRLC')
 union all
 -- 3 loans offered 
 select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[0]:instructions[2]:parsed:info:destination as escrow_token
 from solana.core.fact_transactions
-where contains (log_messages[1],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
-and succeeded
-and signers[3] is not null
-and signers[4] is null
-and instructions[0]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
-union all
-select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[1]:instructions[2]:parsed:info:destination as escrow_token
-from solana.core.fact_transactions
-where contains (log_messages[1],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
-and succeeded
-and signers[3] is not null
-and signers[4] is null
-and instructions[0]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
-union all
-select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[2]:instructions[2]:parsed:info:destination as escrow_token
-from solana.core.fact_transactions
-where contains (log_messages[1],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
-and succeeded
-and signers[3] is not null
-and signers[4] is null
-and instructions[0]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
-union all
--- 3 loans offered (reverse ordering)
-select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[0]:instructions[2]:parsed:info:destination as escrow_token
-from solana.core.fact_transactions
 where contains (log_messages[5],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
+and block_timestamp > current_date() - interval '{{day}} day'
 and succeeded
 and signers[3] is not null
 and signers[4] is null
-and instructions[2]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
+and instructions[2]:accounts[6] = '5274Bqe6RmGkxw6E6M7pYpEkJFh4VS7mcZQe2mQ6ZRLC'
 union all
 select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[1]:instructions[2]:parsed:info:destination as escrow_token
 from solana.core.fact_transactions
 where contains (log_messages[5],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
+and block_timestamp > current_date() - interval '{{day}} day'
 and succeeded
 and signers[3] is not null
 and signers[4] is null
-and instructions[2]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
+and instructions[2]:accounts[6] = '5274Bqe6RmGkxw6E6M7pYpEkJFh4VS7mcZQe2mQ6ZRLC'
 union all
 select tx_id, block_timestamp as datetime, signers[0] as lender, inner_instructions[2]:instructions[2]:parsed:info:destination as escrow_token
 from solana.core.fact_transactions
 where contains (log_messages[5],'OfferLoan')
-and block_timestamp > current_date() - interval '{{hours}} hours'
+and block_timestamp > current_date() - interval '{{day}} day'
 and succeeded
 and signers[3] is not null
 and signers[4] is null
-and instructions[2]:accounts[6] = 'BENrqx18n8tP2xYPBQ7vaDz88Y7wSmzB1xkr8h4XrhvM'
+and instructions[2]:accounts[6] = '5274Bqe6RmGkxw6E6M7pYpEkJFh4VS7mcZQe2mQ6ZRLC'
 
 ), 
 bal as (
@@ -91,8 +44,9 @@ from solana.core.fact_sol_balances
 where account_address in (select txs.escrow_token from txs)
 group by 1
 )
-select amount, lender, datetime --, tx_id
+select amount, concat(substring(lender,1,3)||'___'||substring(lender,42,44)) as user_readable, datetime
 from txs
 join bal on bal.account_address = txs.escrow_token
+where amount > 0.01
 order by 1 desc
 ;
