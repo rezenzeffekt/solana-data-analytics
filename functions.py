a32899bc7d9a18544b77f9431d0d32045f064409
Bert Hoffmann <rezenzeffekt@gmail.com>
2025-05-25 21:05:11 +0200

    funtion input types defined

--- functions.py
+++ functions.py
@@ -1,14 +1,14 @@
 # functions
-def write_list(input,output_path):
+def write_list(input:list,output_path:str):
     """
-    saves a list (input) to a txt file (output_path), confirmation output currently commented out, 
+    saves a list (input) to a txt file (output_path), confirmation output currently commented out, no return values 
     """    
     with open(output_path, 'w') as content:
         content.writelines('\n'.join(input))
     #print(f'list exported to {output_path}')    
-def open_list(input,file_path):
+def open_list(input:list,file_path:str):
     """
-    opens a txt file from path (file_path) as a pre-defined list name (input), list output currently commented out, confirmation output currently commented out
+    opens a txt file from path (file_path) as a pre-defined list name (input), list output currently commented out, confirmation output currently commented out, no return values 
     """    
     with open(file_path, 'r') as content:
         for line in content:
