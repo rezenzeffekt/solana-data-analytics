9a444b8f3ae6a594258e99447465f34afabda3e5
Bert Hoffmann <rezenzeffekt@gmail.com>
2025-05-28 19:07:10 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ def write_list(input_list:list,filename:str):
     open(filename,'w').writelines('\n'.join(input_list))  
 def open_list(filename:str,output_list:list):
     """
-    opens a txt file from path (filename) as a pre-defined list name (output_list), appended list is returned 
+    opens a txt file (filename) into a pre-defined list (output_list), appended list is returned 
     """    
     for line in open(filename, 'r'):
-        output_list.append(line.replace('\n',''))  
+        output_list.append(line.replace('\n',''))   
